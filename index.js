896dbd45c27acb6f17670005b73eef543bb280e8
brokenSensor <jonimen95@yandex.ru>
2021-03-06 18:29:17 +0800

    Auth route

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import express from 'express';
+import DBconnect from './utils/DBconnect.js';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+DBconnect();
+
+app.use(express.json({ extended: false }));
+
+//Routes
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ routes/api/auth.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// @route   POST api/auth
+// @desc    Create user
+// @access  Public
+router.post('/', (req, res) => {});
