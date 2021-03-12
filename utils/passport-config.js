422f1d189d70ad59d5d001ccd3ce0b3d6ac7734d
brokenSensor <jonimen95@yandex.ru>
2021-03-12 15:31:27 +0800

    Google oauth strategy

--- utils/passport-config.js
+++ utils/passport-config.js
@@ -1,4 +1,6 @@
 import { Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import mongoose from 'mongoose';
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
 
@@ -28,6 +30,40 @@ export default passport => {
 			}
 		)
 	);
+
+	passport.use(
+		new GoogleStrategy(
+			{
+				clientID: process.env.GOOGLE_CLIENT_ID,
+				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+				callbackURL: '/api/auth/google/callback',
+			},
+			async (accessToken, refreshToken, profile, done) => {
+				const newUser = {
+					providerId: profile.id,
+					provider: profile.provider,
+					username: profile.displayName,
+					avatar: profile.photos[0].value,
+				};
+
+				try {
+					let user = await User.findOne({
+						provider: profile.provider,
+						providerId: profile.id,
+					});
+					if (user) {
+						done(null, user);
+					} else {
+						user = await User.create(newUser);
+						done(null, user);
+					}
+				} catch (error) {
+					console.log(error);
+				}
+			}
+		)
+	);
+
 	passport.serializeUser((user, done) => {
 		done(null, user.id);
 	});
