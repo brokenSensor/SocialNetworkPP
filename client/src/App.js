889f754e5f528c783035e3ba427ad2d3a813d52e
brokenSensor <jonimen95@yandex.ru>
2021-03-23 11:15:34 +0800

    Register ana login routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,25 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 import Navbar from './components/layout/Navbar';
+import { useEffect } from 'react';
+import { loadUser } from './actions/auth';
 
 function App() {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
 	return (
 		<Provider store={store}>
 			<BrowserRouter>
 				<Navbar />
-				<Route exact path='/' component={''} />
+				<section className='main'>
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/register' component={Register} />
+				</section>
 				{/* <Footer /> */}
 			</BrowserRouter>
 		</Provider>
