1713b9af08c9eb9a50fdb796d85006b521b77000
brokenSensor <jonimen95@yandex.ru>
2021-03-23 08:36:27 +0800

    App set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import { Provider } from 'react-redux';
+import store from './store';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+
 function App() {
-	return <div></div>;
+	return (
+		<Provider store={store}>
+			<BrowserRouter>
+				<Navbar />
+				<Route exact path='/' component={''} />
+				{/* <Footer /> */}
+			</BrowserRouter>
+		</Provider>
+	);
 }
 
 export default App;
