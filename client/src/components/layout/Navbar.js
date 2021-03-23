d52b96868852961ca467330c73b5069e4be61149
brokenSensor <jonimen95@yandex.ru>
2021-03-23 11:16:56 +0800

    Na bar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.sass';
 
 function Navbar() {
 	return (
-		<nav>
+		<nav className='navbar'>
 			<Link to='/profile'>My profile</Link>
 			<Link to='/dashboard'>Dashboard</Link>
 		</nav>
