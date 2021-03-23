4d86ac394dde52d7560f1299e1f2f8e3d6079e4b
brokenSensor <jonimen95@yandex.ru>
2021-03-23 14:51:52 +0800

    Navbar work

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.sass';
+import { logout } from '../../actions/auth';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-function Navbar() {
+function Navbar({ logout }) {
 	return (
 		<nav className='navbar'>
 			<Link to='/profile'>My profile</Link>
 			<Link to='/dashboard'>Dashboard</Link>
+			<a href='#!' onClick={logout}>
+				Logout
+			</a>
 		</nav>
 	);
 }
 
-export default Navbar;
+Navbar.propTypes = {
+	logout: PropTypes.func.isRequired,
+};
+
+export default connect(null, { logout })(Navbar);
