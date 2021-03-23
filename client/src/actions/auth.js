c612d93b60fa583d09682203517bc2ab52cbc02b
brokenSensor <jonimen95@yandex.ru>
2021-03-23 14:50:37 +0800

    Login action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
-import { LOGIN_SUCCESS, REGISTER_SUCCESS, USER_LOADED } from './types.js';
+import {
+	LOGIN_SUCCESS,
+	REGISTER_SUCCESS,
+	USER_LOADED,
+	LOGOUT,
+	AUTH_ERROR,
+	REGISTER_FAIL,
+	LOGIN_FAIL,
+} from './types';
+import { setAlert } from './alert';
 
 // Load user
 export const loadUser = () => async dispatch => {
@@ -12,9 +21,19 @@ export const loadUser = () => async dispatch => {
 		});
 	} catch (error) {
 		dispatch({
-			// type: AUTH_ERROR,
+			type: AUTH_ERROR,
 		});
 	}
+};
+// Logout user
+export const logout = () => async dispatch => {
+	try {
+		await axios.get('/api/auth/logout');
+
+		dispatch({
+			type: LOGOUT,
+		});
+	} catch (error) {}
 };
 
 // Register user
@@ -26,12 +45,50 @@ export const register = ({ username, email, password }) => async dispatch => {
 	};
 
 	const body = JSON.stringify({ username, email, password });
-	dispatch(loadUser());
+
 	try {
-		const res = await axios.post('/api/auth/create', body, config);
+		await axios.post('/api/auth/create', body, config);
 		dispatch({
 			type: REGISTER_SUCCESS,
-			payload: res.data,
 		});
-	} catch (error) {}
+		dispatch(loadUser());
+	} catch (error) {
+		const err = error.response.data.errors;
+
+		if (err) {
+			err.forEach(er => dispatch(setAlert(er.msg, 'danger')));
+		}
+
+		dispatch({
+			type: REGISTER_FAIL,
+		});
+	}
+};
+
+// Login user
+export const login = (email, password) => async dispatch => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+
+	const body = JSON.stringify({ email, password });
+	try {
+		const res = await axios.post('/api/auth/local', body, config);
+		dispatch({
+			type: LOGIN_SUCCESS,
+		});
+		dispatch(loadUser());
+	} catch (error) {
+		const err = error.response.data.errors;
+
+		if (err) {
+			err.forEach(er => dispatch(setAlert(er.msg, 'danger')));
+		}
+
+		dispatch({
+			type: LOGIN_FAIL,
+		});
+	}
 };
