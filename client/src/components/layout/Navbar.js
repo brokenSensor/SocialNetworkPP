ad2b37b8aa32d6d5315214f3ec711bede82410da
brokenSensor <jonimen95@yandex.ru>
2021-03-26 10:13:55 +0800

    Hide nav if not loged in

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,20 +5,28 @@ import { logout } from '../../actions/auth';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-function Navbar({ logout }) {
+function Navbar({ auth: { isAuthenticated }, logout }) {
+	console.log(isAuthenticated);
 	return (
-		<nav className='navbar'>
-			<Link to='/profile'>My profile</Link>
-			<Link to='/dashboard'>Dashboard</Link>
-			<a href='#!' onClick={logout}>
-				Logout
-			</a>
-		</nav>
+		isAuthenticated && (
+			<nav className='navbar'>
+				<Link to='/profile'>My profile</Link>
+				<Link to='/dashboard'>Dashboard</Link>
+				<a href='#!' onClick={logout}>
+					Logout
+				</a>
+			</nav>
+		)
 	);
 }
 
 Navbar.propTypes = {
 	logout: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
 };
 
-export default connect(null, { logout })(Navbar);
+const mapStateToProps = state => ({
+	auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
