1fbea704ce20912804c10f735d28de105d0d3e3c
brokenSensor <jonimen95@yandex.ru>
2021-03-26 10:46:36 +0800

    PROFILE_LOADED PROFILE_ERROR types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,3 +7,5 @@ export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const PROFILE_LOADED = 'PROFILE_LOADED';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
