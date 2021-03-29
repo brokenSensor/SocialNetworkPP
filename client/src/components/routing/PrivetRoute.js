38dd51e69202f4d4f3e9e55f53a495b0ec722f4f
brokenSensor <jonimen95@yandex.ru>
2021-03-29 09:13:23 +0800

    PrivetRoute

---
+++ client/src/components/routing/PrivetRoute.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect, Route } from 'react-router';
+
+function PrivetRoute({
+	component: Component,
+	auth: { isAuthenticated, loading },
+	...rest
+}) {
+	return (
+		<Route
+			{...rest}
+			render={props =>
+				!isAuthenticated && !loading ? (
+					<Redirect to='/login' />
+				) : (
+					<Component {...props} />
+				)
+			}
+		/>
+	);
+}
+
+PrivetRoute.propTypes = {
+	auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+	auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivetRoute);
