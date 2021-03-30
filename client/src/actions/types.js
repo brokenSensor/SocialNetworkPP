568c193247423d8fbd5960649e8bd57947d6ab23
brokenSensor <jonimen95@yandex.ru>
2021-03-30 11:20:04 +0800

    PROFILE_EDITED

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -9,3 +9,4 @@ export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const PROFILE_LOADED = 'PROFILE_LOADED';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
+export const PROFILE_EDITED = 'PROFILE_EDITED';
