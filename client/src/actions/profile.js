5edfa0b75501343870dc358f1c9ebf67ba8ff855
brokenSensor <jonimen95@yandex.ru>
2021-03-30 11:19:56 +0800

    Create profile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { PROFILE_LOADED, PROFILE_ERROR } from './types';
+import { PROFILE_LOADED, PROFILE_ERROR, PROFILE_EDITED } from './types';
+import { setAlert } from './alert';
 
 export const loadProfile = userId => async dispatch => {
 	try {
@@ -10,6 +11,32 @@ export const loadProfile = userId => async dispatch => {
 			payload: res.data,
 		});
 	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+		});
+	}
+};
+
+export const createProfile = profileForm => async dispatch => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+	const body = JSON.stringify(profileForm);
+	try {
+		const res = await axios.post(`/api/profile/create`, body, config);
+
+		dispatch({
+			type: PROFILE_EDITED,
+			payload: res.data,
+		});
+	} catch (error) {
+		const err = error.response?.data?.errors;
+
+		if (err) {
+			err.forEach(er => dispatch(setAlert(er.msg, 'danger')));
+		}
 		dispatch({
 			type: PROFILE_ERROR,
 		});
